61cd9a8288e762e817b706e669dd217ac9de99b5
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-01-22 12:03:37 +0530

    Create userinput .java

---
+++ userinput .java
@@ -0,0 +1,16 @@
+package program1;
+
+import java.util.Scanner;
+
+public class userinput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        x=scan.nextInt();
+        System.out.println("SUM ="+x);
+	}
+
+}
