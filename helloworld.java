d5b47c13f16f4a0b0b22668bfcd9bd74eb37c505
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-01-22 12:02:45 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+/*
+ * This is my first program
+ */
+package program1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
