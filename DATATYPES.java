131494475b0cc9cb36b572030f3644fa324504bf
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-01-22 12:02:11 +0530

    Create  DATATYPES.java

---
+++ DATATYPES.java
@@ -0,0 +1,19 @@
+package program1;
+
+public class DATATYPES {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		float y=(float) 4.5;
+		double z=11.78;
+		char a='B';
+		boolean b=false;
+		System.out.println(b);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(a);
+		System.out.println(z);
+	}
+
+}
