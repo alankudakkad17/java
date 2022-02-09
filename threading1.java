5759a85e928eb913778aafb15fe79a217c97e81b
ALAN ANTO <89739433+alankudakkad17@users.noreply.github.com>
2022-02-09 21:37:57 +0530

    Create threading1.java

---
+++ threading1.java
@@ -0,0 +1,51 @@
+package program1;
+import java.util.Scanner;
+class b extends Thread{
+	public void run() {
+		
+			for(int i=1;i<=5;i++) {
+				System.out.println(i);
+				try {
+					Thread.sleep(400);
+				}
+				catch(Exception e){
+					System.out.println(e);
+				}
+			}
+  }
+}	
+class a  extends Thread{
+
+	public void run() {
+		for(char i='a';i<='e';i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(400);
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
+	}
+	
+}
+}
+public class Muli_threading_jion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		a a1=new a();
+		b b1=new b();
+	    try {
+	    	a1.start();
+	    	b1.start();
+	    	a1.join();
+	    	b1.join();
+	    }
+	    catch(Exception e) {
+	    	System.out.println(e);
+	    }
+	    
+
+	}
+
+}
